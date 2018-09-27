261742970d666977f9a7e3a86a56c17b635cee45
Gyraxy <nicolasduboscq@hotmail.com>
2018-09-27 10:13:37 -1000

    Added : Dimen files AuthActivity xml creation for first authentification window Color Style modification (Primary/Secondary in orange and accent in green) Styles modification for theme with no action bar

--- app/src/main/java/com/duboscq/nicolas/go4lunch/controllers/activities/MainActivity.java
+++ app/src/main/java/com/duboscq/nicolas/go4lunch/controllers/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.duboscq.nicolas.go4lunch;
+package com.duboscq.nicolas.go4lunch.controllers.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.duboscq.nicolas.go4lunch.R;
 
 public class MainActivity extends AppCompatActivity {
 
