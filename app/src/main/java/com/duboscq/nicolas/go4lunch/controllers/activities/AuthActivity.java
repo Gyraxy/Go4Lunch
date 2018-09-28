d62db76c3adb4cae289802605173dfca29ee1841
Gyraxy <nicolasduboscq@hotmail.com>
2018-09-28 11:01:43 -1000

    Added : Facebook & Google Authentification Dimens for Authentification Layout

---
+++ app/src/main/java/com/duboscq/nicolas/go4lunch/controllers/activities/AuthActivity.java
@@ -0,0 +1,116 @@
+package com.duboscq.nicolas.go4lunch.controllers.activities;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.duboscq.nicolas.go4lunch.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AuthActivity extends AppCompatActivity {
+
+    //FOR DESIGN
+    @BindView(R.id.login_facebook_imv)
+    ImageView login_facebook_btn;
+    @BindView(R.id.login_google_imv)
+    ImageView login_google_btn;
+
+    //FOR DATA
+    private static final int RC_SIGN_IN = 123;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+        ButterKnife.bind(this);
+    }
+
+    //ACTIONS
+
+    @OnClick(R.id.login_facebook_imv)
+    public void onClickFacebookLogin() {
+        if (isCurrentUserLogged()){
+            startMainActivity();
+        } else {
+            startSignInFacebook();
+        }
+    }
+
+    @OnClick(R.id.login_google_imv)
+    public void onClickGoogleLogin() {
+        if (isCurrentUserLogged()){
+            startMainActivity();
+        } else {
+            startSignInGooglePlus();
+        }
+    }
+
+    public void startMainActivity() {
+        Intent login = new Intent(this, MainActivity.class);
+        startActivity(login);
+    }
+
+
+    // ----------------
+    // AUTHENTIFICATION
+    // ----------------
+
+    // Authentication mode: Google
+    private void startSignInGooglePlus(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    // Authentication mode: Facebook
+    private void startSignInFacebook(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(
+                                        new AuthUI.IdpConfig.FacebookBuilder().build()))
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    // --------------------
+    // UTILS
+    // --------------------
+
+    @Nullable
+    protected FirebaseUser getCurrentUser(){ return FirebaseAuth.getInstance().getCurrentUser(); }
+
+    protected Boolean isCurrentUserLogged(){ return (this.getCurrentUser() != null); }
+
+}
--- app/src/main/java/com/duboscq/nicolas/go4lunch/controllers/activities/MainActivity.java
+++ app/src/main/java/com/duboscq/nicolas/go4lunch/controllers/activities/MainActivity.java
@@ -1,15 +1,62 @@
 package com.duboscq.nicolas.go4lunch.controllers.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.duboscq.nicolas.go4lunch.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Created by Nicolas DUBOSCQ on 27/09/2018
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.logout)Button logout;
+
+    private static final int SIGN_OUT_TASK = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    // --------------------
+    // ACTIONS
+    // --------------------
+
+    @OnClick(R.id.logout)
+    public void onClickSignOutButton() {
+        this.signOutUserFromFirebase();
+    }
+
+    private void signOutUserFromFirebase() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnSuccessListener(this, this.updateUIAfterRESTRequestsCompleted(SIGN_OUT_TASK));
+    }
+
+    private OnSuccessListener<Void> updateUIAfterRESTRequestsCompleted(final int origin){
+        return new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                switch (origin){
+                    case SIGN_OUT_TASK:
+                        finish();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        };
     }
 }
